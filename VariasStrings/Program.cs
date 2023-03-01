f906aa5bb6bc562437bfa5fd165481cae18b4f3a
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-01 17:09:54 +0000

    Add more strings to VariasStrings project

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,12 +6,29 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
+            int x = 10;
+            int y = -10;
+            string myStr;
+
             Console.WriteLine("Hello, World!");
             Console.WriteLine(@"\nHello, World!\n");
             Console.WriteLine("\u00A9 \u2703");
             Console.WriteLine(@"\u00A9 \u2703");
             Console.WriteLine("\"This is in double quotes.\"");
             Console.WriteLine(@"""This is in double quotes.""");
+
+            Console.WriteLine("a" + 2);
+            Console.WriteLine("abc" + x);
+
+            myStr = $"Valor de x é {x}.";
+            Console.WriteLine(myStr);
+            myStr = $"{x} mais {y} é igual a {x + y}.";
+            Console.WriteLine(myStr);
+
+            myStr = string.Format("Y é {1}, X é {0}.", x, y);
+            Console.WriteLine(myStr);
+
+            Console.WriteLine(@"Verbatim com x = {0}.", x);
         }
     }
 }
