ea5369b4a2ba1c6cd382168c4b372ba46f313660
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-01 16:55:26 +0000

    Add VariasStrings project

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(@"\nHello, World!\n");
+            Console.WriteLine("\u00A9 \u2703");
+            Console.WriteLine(@"\u00A9 \u2703");
+            Console.WriteLine("\"This is in double quotes.\"");
+            Console.WriteLine(@"""This is in double quotes.""");
+        }
+    }
+}
