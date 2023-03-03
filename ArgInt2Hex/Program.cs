36e36fc1d9380e359071c853e32d44c6d8aafeae
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-03 11:34:15 +0000

    Add ArgInt2Hex project

---
+++ ArgInt2Hex/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArgInt2Hex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int itemInt;
+            bool condition = false;
+
+            foreach (var item in args)
+            {
+                condition = int.TryParse(item, out itemInt);
+
+                if (condition)
+                {
+                    Console.WriteLine(
+                        $"{itemInt} -> É inteiro, valor hexadecimal: {itemInt:x}");
+                }
+
+                else
+                {
+                    Console.WriteLine("{0} -> Não é inteiro", item);
+                }
+            }
+        }
+    }
+}
